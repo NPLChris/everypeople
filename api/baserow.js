5ce28adce013b47727788edbdc85f1ce87fb0a23
NPLChris <clynch2414@gmail.com>
2025-07-12 16:29:46 -0500

    Update baserow.js

--- api/baserow.js
+++ api/baserow.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const allowedOrigins = [
-    'https://www.everypeople.org',
+    'https://www.everypeople.org/test',
   ];
 
   const origin = req.headers.origin;
